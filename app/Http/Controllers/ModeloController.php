55df1205d8ab84a0150f2b3cd3f33b70d591dcb4
Ana_Julia_Costa <113532608+A-juli07@users.noreply.github.com>
2025-08-19 15:56:19 -0400

    Adiciona rotas do Swagger

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -15,6 +15,46 @@ class ModeloController extends Controller
         $this->modelo = $modelo;
     }
 
+    /**
+     * @OA\Get(
+     *   path="/api/v1/modelo",
+     *   summary="Modelo cadastrados",
+     *   tags={"Modelo"},
+     *   security={{"bearerAuth":{}}},
+     * 
+     *  @OA\Parameter(
+     *     name="atributos_marca",
+     *     in="query",
+     *     required=false,
+     *     description="Quais colunas do relacionamento 'marca' retornar. Use vírgula para separar.",
+     *     example="nome,imagem",
+     *     @OA\Schema(type="string")
+     *   ),
+     * 
+     *   @OA\Parameter(
+     *     name="filtro",
+     *     in="query",
+     *     required=false,
+     *     description="Filtros dinâmicos. Exemplos de operadores comuns: =, >, >=, <, <=, like, in, between. ",
+     *     example="nome:like:Fiat;numero_portas:<:4",
+     *     @OA\Schema(type="string")
+     *   ),
+     * 
+     *   @OA\Parameter(
+     *     name="atributos",
+     *     in="query",
+     *     required=false,
+     *     description="Quais colunas do recurso 'modelo' retornar. Use vírgula para separar.",
+     *     example="nome,imagem,numero_portas,lugares,marca_id",
+     *     @OA\Schema(type="string")
+     *   ),
+     * 
+     *   @OA\Response(response=200, description="Retorna os dados do modelo"),
+     *   @OA\Response(response=401, description="Não autenticado")
+     * )
+     */
+    
+    
     public function index(Request $request)
     {
         $modeloRepository = new ModeloRepository($this->modelo);
@@ -38,6 +78,54 @@ class ModeloController extends Controller
         return response()->json($modeloRepository->getResultado(), 200);
     }
 
+
+    /**
+     * @OA\Post(
+     *   path="/api/v1/modelo",
+     *   summary="Cadastrar modelo",
+     *   tags={"Modelo"},
+     *   security={{"bearerAuth":{}}},
+     *
+     *   @OA\RequestBody(
+     *     required=true,
+     *     @OA\MediaType(
+     *       mediaType="multipart/form-data",
+     *       @OA\Schema(
+     *         type="object",
+     *         required={"nome","imagem","marca_id","numero_portas","lugares","air_bag","abs"},
+     *         @OA\Property(
+     *           property="nome",
+     *           type="string",
+     *           minLength=3,
+     *           description="Nome do modelo.",
+     *           example="Tesla"
+     *         ),
+     *         @OA\Property(
+     *           property="imagem",
+     *           type="string",
+     *           format="binary",
+     *           description="Arquivo de imagem (formatos permitidos: png, jpg, svg, webp)."
+     *         ),
+     *         @OA\Property(property="marca_id", type="integer", example="1", description="ID do modelo."),
+     *         @OA\Property(property="numero_portas", type="integer", example="4", description="Numero de portas do modelo"),
+     *         @OA\Property(property="lugares", type="integer", example="5", description="Quantidade de lugares"),
+     *         @OA\Property(property="air_bag", type="text", example="1", description="Air_bag disponivel?"),
+     *         @OA\Property(property="abs", type="text", example="1", description="abs disponivel?")
+     *       )
+     *     )
+     *   ),
+     *
+     *   @OA\Response(
+     *     response=201,
+     *     description="Marca criada com sucesso"),
+     *
+     *   @OA\Response(
+     *     response=422,
+     *     description="Erros de validação")
+     * )
+     */
+       
+
     public function store(Request $request)
     {
         $request->validate($this->modelo->rules());
@@ -59,6 +147,28 @@ class ModeloController extends Controller
 
         return response()->json($modelo, 201);
     }
+
+
+    /**
+     * @OA\Get(
+     *   path="/api/v1/modelo/{id}",
+     *   summary="Detalhes de uma modelo",
+     *   tags={"Modelo"},
+     *   security={{"bearerAuth":{}}},
+     *
+     *   @OA\Parameter(
+     *     name="id",
+     *     in="path",
+     *     required=true,
+     *     description="ID do modelo a ser pesquisado",
+     *     @OA\Schema(type="integer"),
+     *     example=1
+     *   ),
+     * 
+     *   @OA\Response(response=404, description="Modelo não encontrado"),
+     *   @OA\Response(response=422, description="Dados inválidos")
+     * )
+     */
 
     public function show($id)
     {
@@ -111,6 +221,29 @@ class ModeloController extends Controller
         return response()->json($modelo, 200);
     }
 
+
+    /**
+     * @OA\Delete(
+     *   path="/api/v1/modelo/{id}",
+     *   summary="Remover um modelo",
+     *   tags={"Modelo"},
+     *   security={{"bearerAuth":{}}},
+     *
+     *   @OA\Parameter(
+     *     name="id",
+     *     in="path",
+     *     required=true,
+     *     description="ID do modelo a ser deletado",
+     *     @OA\Schema(type="integer"),
+     *     example=2
+     *   ),
+     * 
+     *   @OA\Response(response=404, description="Modelo não encontrado"),
+     *   @OA\Response(response=422, description="Dados inválidos")
+     * )
+     */
+
+
     public function destroy($id)
     {
         $modelo = $this->modelo->find($id);
@@ -122,6 +255,6 @@ class ModeloController extends Controller
         Storage::disk('public')->delete($modelo->imagem);
 
         $modelo->delete();
-        return response()->json(['msg' => 'O modelo foi removida com sucesso!'], 200);
+        return response()->json(['msg' => 'O modelo foi removido com sucesso!'], 200);
     }
 }
